4f8f210f535315508dc86f196df808a84673fb31
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-19 13:08:47 -0700

    add test for mercuryAge method

--- _tests_/SpaceAges.test.js
+++ _tests_/SpaceAges.test.js
@@ -1,9 +1,17 @@
 import SpaceAges from '../src/js/SpaceAges.js'
 
 describe('SpaceAges', () => {
+  let testAge;
+
+  beforeEach(() => {
+    testAge = new SpaceAges(30);
+  });
 
   test('should correctly create a spaceAges object with earth age', () => {
-    const testAge = new SpaceAges(30);
     expect(testAge.earthAge).toEqual(30);
   });
+  
+  test('should correctly calculate mercuryAge based on EarthAge', () => {
+    expect(testAge.calculateMercuryAge()).toEqual(7.2);
+  })
 })

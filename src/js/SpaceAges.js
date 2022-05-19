d7d1fccfd2339419a2b8892155f81234735014f1
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-19 13:29:29 -0700

    pass venusAge test, add marsAge test

--- src/js/SpaceAges.js
+++ src/js/SpaceAges.js
@@ -2,10 +2,16 @@ export default class SpaceAges {
   constructor(earthAge) {
     this.earthAge = earthAge;
     this.mercuryAge;
+    this.venusAge;
   };
 
   calculateMercuryAge() {
     this.mercuryAge = (Math.round(this.earthAge * .24));
-    return this.mercuryAge;
+    return this.mercuryAge
   };
+
+  calculateVenusAge() {
+    this.venusAge = (Math.round(this.earthAge * .62));
+    return this.venusAge
+  }
 }

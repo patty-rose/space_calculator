d96c5b78548fc16fd27963dfa2cf616a4687cf78
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-19 22:12:47 -0700

    pass calcPlanetaryAge test

--- _tests_/calculators.test.js
+++ _tests_/calculators.test.js
@@ -1,5 +1,32 @@
-import Calculators from '../src/js/calculators.js'
-import Person from '../src/js/person';
+import {Person, Calculators} from '../src/js/calculators.js';
+
+describe('Person', () => {
+  let testPerson;
+  
+  beforeEach(() => {
+    testPerson = new Person(30, false, false, false, true, true, true);
+  });
+
+  test('should correctly create a Person object with Earth age', () => {
+    expect(testPerson.earthAge).toEqual(30);
+  });
+
+  test('should correctly calculate mercuryAge based on earthAge', () => {
+    expect(testPerson.calculateMercuryAge()).toEqual(7);
+  });
+  test('should correctly calculate venusAge based on earthAge', () => {
+    expect(testPerson.calculateVenusAge()).toEqual(19);
+  });
+  test('should correctly calculate marsAge based on earthAge', () => {
+    expect(testPerson.calculateMarsAge()).toEqual(56);
+  });
+  test('should correctly calculate jupiterAge based on earthAge', () => {
+    expect(testPerson.calculateJupiterAge()).toEqual(356);
+  });
+  test('should determine how many years a user has left to live on Earth', () => {
+    expect(testPerson.calculateEarthYearsLeft()).toEqual(43);
+  });
+});
 
 describe('Calculators', () => {
   let calculator;
@@ -7,11 +34,11 @@ describe('Calculators', () => {
 
   beforeEach(() => {
     testPerson = new Person(30, false, false, false, true, true, true);
-    calculator = new Calculators("testPerson");
+    calculator = new Calculators(testPerson);
   })
 
   test('should create calculator object', () => {
-    expect(calculator.person).toEqual("testPerson")
+    expect(calculator.person).toEqual(testPerson)
   });
 
   test('should calculate planet solar years proportion to Earth', () => {
--- src/js/calculators.js
+++ src/js/calculators.js
@@ -1,6 +1,53 @@
-import Person from '../src/js/person'; 
+export class Person {
+  constructor(earthAge, isSmoker, sleepsWell, isRich, eatsFish, hasInboxZero, wearsTallSocks) {
+    this.earthAge = earthAge;
+    this.isSmoker = isSmoker;
+    this.sleepsWell = sleepsWell;
+    this.isRich = isRich;
+    this.eatsFish = eatsFish;
+    this.hasInboxZero = hasInboxZero;
+    this.wearsTallSocks = wearsTallSocks;
+  }
 
-export default class Calculators {
+  calculateMercuryAge() {
+    this.mercuryAge = (Math.round(this.earthAge * .24));
+    return this.mercuryAge
+  }
+
+  calculateVenusAge() {
+    this.venusAge = (Math.round(this.earthAge * .62));
+    return this.venusAge
+  }
+
+  calculateMarsAge() {
+    this.marsAge = (Math.round(this.earthAge * 1.88));
+    return this.marsAge
+  }
+
+  calculateJupiterAge() {
+    this.jupiterAge = (Math.round(this.earthAge * 11.86));
+    return this.jupiterAge
+  }
+
+  calculateEarthYearsLeft() {
+    let earthYearsLeft;
+    let yearsToLive = 72;
+    (this.isSmoker === true) ? yearsToLive -= 1 : yearsToLive += 1;
+    (this.sleepsWell === true) ? yearsToLive += 3 : yearsToLive -= 3;
+    (this.isRich === true) ? yearsToLive += 6 : yearsToLive -= 6;
+    (this.eatsFish === true) ? yearsToLive += 2 : yearsToLive -= 2;
+    (this.hasInboxZero === true) ? yearsToLive += 4 : yearsToLive -= 4;
+    (this.wearsTallSocks === true) ? yearsToLive += 3 : yearsToLive -= 3;
+    this.earthYearsExpected = yearsToLive;
+    if (yearsToLive > this.earthAge) {
+      earthYearsLeft = (yearsToLive - this.earthAge);
+    } else {
+      earthYearsLeft = (this.earthAge - yearsToLive);
+    } return earthYearsLeft
+  }
+}
+
+export class Calculators {
   constructor(person) {
     this.person = person;
   }
@@ -16,5 +63,11 @@ export default class Calculators {
     };
     planetProportion = planetYearsMap[planet];
     return planetProportion
+  }
+
+  calcPlanetaryAge(planet) {
+    const planetProportion = this.calcPlanetProportion(planet);
+    const planetaryAge = (Math.round(this.person.earthAge * planetProportion));
+    return planetaryAge
   }
 }

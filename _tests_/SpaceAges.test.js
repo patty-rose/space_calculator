9f156907afee9a2de2414b7aa820f491699434c1
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-19 21:29:18 -0700

    add Calculators class tests: constructor and planetProportion

--- _tests_/SpaceAges.test.js
+++ _tests_/SpaceAges.test.js
@@ -1,4 +1,23 @@
 import SpaceAges from '../src/js/SpaceAges.js'
+describe('Calculators', () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new Calculators();
+  })
+
+  test('should create calculator object', () => {
+    expect(calculator).toEqual(calculator)
+  });
+
+  test('should calculate planet solar years proportion to Earth', () => {
+    expect(calculator.calcPlanetProportion("earth")).toEqual(1);
+    expect(calculator.calcPlanetProportion("mercury")).toEqual(.24);
+    expect(calculator.calcPlanetProportion("venus")).toEqual(.62);
+    expect(calculator.calcPlanetProportion("mars")).toEqual(1.88);
+    expect(calculator.calcPlanetProportion("jupiter")).toEqual(11.86);
+  });
+})
 
 describe('SpaceAges', () => {
   let testAge;
@@ -7,7 +26,7 @@ describe('SpaceAges', () => {
     testAge = new SpaceAges(30, false, false, false, true, true, true);
   });
 
-  test('should correctly create a spaceAges object with earth age', () => {
+  test('should correctly create a SpaceAges object with Earth age', () => {
     expect(testAge.earthAge).toEqual(30);
   });
   
